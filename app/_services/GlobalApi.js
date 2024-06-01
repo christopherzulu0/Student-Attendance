0fa438e6e586e58385efb8d1a82f47be7bfb8c3e
christopherzulu0 <christopherzulu04@gmail.com>
2024-06-01 17:20:11 +0200

    intermission

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -1,8 +1,12 @@
  const {default: axios}= require("axios");
 
  const GetAllGrades = ()=>axios.get('/api/grades');
+ const GetAllStudents = ()=>axios.get('/api/students');
  const CreateNewStudent =(data)=>axios.post('/api/students',data)
+ 
+ 
  export default{
     GetAllGrades,
+    GetAllStudents,
     CreateNewStudent
  }
--- app/dashboard/students/page.js
+++ app/dashboard/students/page.js
@@ -1,7 +1,20 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 import AddNewStudent from './_components/AddNewStudent'
+import GlobalApi from '@/app/_services/GlobalApi'
 
 export default function Students() {
+
+    useEffect(()=>{
+    GetAllStudents();
+    })
+    
+    const GetAllStudents = () =>{
+        GlobalApi.GetAllStudents().then(res=>{
+            console.log(res.data)
+        })
+    }
   return (
     <div className='p-7'>
     <h2 className='font-bold text-2xl flex justify-between items-center'>
