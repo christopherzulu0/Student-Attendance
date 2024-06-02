a4b83f5174d50b3a5ca49a58fa644e54b0d7d719
christopherzulu0 <christopherzulu04@gmail.com>
2024-06-02 03:06:10 +0200

    attendance page working

---
+++ app/dashboard/attendance/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Attendance() {
+  return (
+    <div className='p-7'>
+        <h2 className='text-2xl font-bold'>Attendance</h2>
+    </div>
+  )
+}
--- utils/schema.js
+++ utils/schema.js
@@ -1,12 +1,12 @@
 import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
 export const GRADES = mysqlTable('grades',{
-    id:int('id').primaryKey(),
+    id:int('id',{length:11}).primaryKey(),
     grade:varchar('grade',{length:10}).notNull()
 });
 
 export const STUDENTS = mysqlTable('student',{
-    id:int('id').autoincrement().primaryKey(),
+    id:int('id',{length:11}).autoincrement().primaryKey(),
     name:varchar('name',{length:20}).notNull(),
     grade:varchar('grade',{length:10}).notNull(),
     address:varchar('address',{length:500}).notNull(),
