4123d3be9b09f16226612be25a03a78254f5a36f
CHRISTOPHER ZULU <104396388+christopherzulu0@users.noreply.github.com>
2024-09-24 20:11:17 +0200

    Update page.js
    route change

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@
 // import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
 import { redirect } from "next/navigation";
 import React,{useEffect} from "react";
-
+import dashboard from './dashboard'
 export default function Home() {
   useEffect(()=>{
     redirect('/dashboard')
   },[])
   return (
    <>
-   <h1>Twsts</h1>
+  <dashboard/>
    </>
   );
 }
